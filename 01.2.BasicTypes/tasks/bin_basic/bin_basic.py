a8821f5dfbf15d64f97bb22f1dcca45236dac1b0
@Arthur <artur.makarkin2006@gmail.com>
2024-09-22 13:33:17 +0300

    first

--- 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
@@ -1,7 +1,14 @@
 def find_value(nums: list[int] | range, value: int) -> bool:
-    """
-    Find value in sorted sequence
-    :param nums: sequence of integers. Could be empty
-    :param value: integer to find
-    :return: True if value exists, False otherwise
-    """
+    left = -1
+    right = len(nums)
+
+    while right - left > 1:
+        mid = (right + left) // 2
+        if nums[mid] > value:
+            right = mid
+        elif nums[mid] < value:
+            left = mid
+        else:
+            return True
+
+    return False
