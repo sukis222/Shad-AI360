f43d46c538a4059f8295723f15a08501c6db75ab
@Arthur <artur.makarkin2006@gmail.com>
2024-09-22 14:26:31 +0300

    k

--- 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
@@ -1,7 +1,9 @@
 def find_value(nums: list[int] | range, value: int) -> bool:
     left = 0
     right = len(nums)
-
+    if right > 0:
+        if nums[left] == value:
+            return True
     while right - left > 1:
         mid = (right + left) // 2
         if nums[mid] > value:
@@ -12,3 +14,7 @@ def find_value(nums: list[int] | range, value: int) -> bool:
             return True
 
     return False
+
+d = list(map(int, input().split()))
+a = int(input())
+print(find_value(d, a))
