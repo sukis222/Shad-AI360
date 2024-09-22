852067355a2ab73840da5cac5c9939c4398a1c47
@Arthur <artur.makarkin2006@gmail.com>
2024-09-22 16:22:13 +0300

    fizzb

--- 01.2.BasicTypes/tasks/fizz_buzz/fizz_buzz.py
+++ 01.2.BasicTypes/tasks/fizz_buzz/fizz_buzz.py
@@ -1,9 +1,9 @@
 def get_fizz_buzz(n: int) -> list[int | str]:
-    """
-    If value divided by 3 - "Fizz",
-       value divided by 5 - "Buzz",
-       value divided by 15 - "FizzBuzz",
-    else - value.
-    :param n: size of sequence
-    :return: list of values.
-    """
+    nums = [i for i in range(1, n+1)]
+    for i in range(2, n, 3):
+        nums[i] = "Fizz"
+    for i in range(4, n, 5):
+        nums[i] = "Buzz"
+    for i in range(14, n, 15):
+        nums[i] = "FizzBuzz"
+    return nums
