20651e48304c0bfdbe7b0811707aa8d9dde94d9d
@Arthur <artur.makarkin2006@gmail.com>
2024-09-23 10:20:45 +0300

    make the List and Union

--- 01.2.BasicTypes/tasks/fizz_buzz/fizz_buzz.py
+++ 01.2.BasicTypes/tasks/fizz_buzz/fizz_buzz.py
@@ -1,3 +1,6 @@
+from typing import List, Union
+
+
 def get_fizz_buzz(n: int) -> list[int | str]:
     """
         If value divided by 3 - "Fizz",
@@ -7,7 +10,8 @@ def get_fizz_buzz(n: int) -> list[int | str]:
         :param n: size of sequence
         :return: list of values.
         """
-    sequence = [i for i in range(1, n+1)]
+    sequence: List[Union[int, str]] = [i for i in range(1, n+1)]
+
     for i in range(2, n, 3):
         sequence[i] = 'Fizz'
     for i in range(4, n, 5):
@@ -15,4 +19,3 @@ def get_fizz_buzz(n: int) -> list[int | str]:
     for i in range(14, n, 15):
         sequence[i] = 'FizzBuzz'
     return sequence
-
