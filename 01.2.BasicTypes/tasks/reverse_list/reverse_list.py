1edd34b05701100ccadc54a167287523bedbd4f9
@Arthur <artur.makarkin2006@gmail.com>
2024-09-23 10:05:38 +0300

    write comments back

--- 01.2.BasicTypes/tasks/reverse_list/reverse_list.py
+++ 01.2.BasicTypes/tasks/reverse_list/reverse_list.py
@@ -1,4 +1,9 @@
 def reverse_iterative(lst: list[int]) -> list[int]:
+    """
+        Return reversed list. You can use only iteration
+        :param lst: input list
+        :return: reversed list
+        """
     lst2 = []
     for j in range(len(lst) - 1, -1, -1):
         lst2.append(lst[j])
@@ -6,6 +11,12 @@ def reverse_iterative(lst: list[int]) -> list[int]:
 
 
 def reverse_inplace_iterative(lst: list[int]) -> None:
+    """
+       Revert list inplace. You can use only iteration
+       :param lst: input list
+       :return: None
+       """
+
     start = 0
     end = len(lst) - 1
     while start < end:
@@ -15,12 +26,30 @@ def reverse_inplace_iterative(lst: list[int]) -> None:
 
 
 def reverse_inplace(lst: list[int]) -> None:
+    """
+        Revert list inplace with reverse method
+        :param lst: input list
+        :return: None
+        """
+
     lst.reverse()
 
 
 def reverse_reversed(lst: list[int]) -> list[int]:
+    """
+        Revert list with `reversed`
+        :param lst: input list
+        :return: reversed list
+        """
+
     return list(reversed(lst))
 
 
 def reverse_slice(lst: list[int]) -> list[int]:
+    """
+        Revert list with slicing
+        :param lst: input list
+        :return: reversed list
+        """
+
     return lst[::-1]
