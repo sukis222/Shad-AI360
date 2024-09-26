958c530be0a0c11cacfde47cc00de2ac2f420b01
@Arthur <artur.makarkin2006@gmail.com>
2024-09-26 11:29:51 +0300

    first

--- 01.2.BasicTypes/tasks/merge_lists/merge_lists.py
+++ 01.2.BasicTypes/tasks/merge_lists/merge_lists.py
@@ -5,6 +5,26 @@ def merge_iterative(lst_a: list[int], lst_b: list[int]) -> list[int]:
     :param lst_b: second sorted list
     :return: merged sorted list
     """
+    s = []
+    i = 0
+    j = 0
+    while i < len(lst_a) or j < len(lst_b):
+        if i == len(lst_a):
+            s.append(lst_b[j])
+            j += 1
+            continue
+        elif j == len(lst_b):
+            s.append(lst_a[i])
+            i += 1
+            continue
+
+        if lst_a[i] < lst_b[j]:
+            s.append(lst_a[i])
+            i += 1
+        else:
+            s.append(lst_b[j])
+            j += 1
+    return s
 
 
 def merge_sorted(lst_a: list[int], lst_b: list[int]) -> list[int]:
@@ -14,3 +34,4 @@ def merge_sorted(lst_a: list[int], lst_b: list[int]) -> list[int]:
     :param lst_b: second sorted list
     :return: merged sorted list
     """
+    return sorted(lst_a + lst_b)
