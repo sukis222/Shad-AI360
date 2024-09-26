0e11997e923c0f5374f47310805b522dd16f6b10
@Arthur <artur.makarkin2006@gmail.com>
2024-09-26 23:04:09 +0300

    first

--- 01.2.BasicTypes/tasks/iterate_me/iterate_me.py
+++ 01.2.BasicTypes/tasks/iterate_me/iterate_me.py
@@ -3,7 +3,7 @@ def get_squares(elements: list[int]) -> list[int]:
     :param elements: list with integer values
     :return: list with squared values
     """
-    
+    return [i ** 2 for i in elements]
 
 
 # ====================================================================================================
@@ -14,6 +14,7 @@ def get_indices_from_one(elements: list[int]) -> list[int]:
     :param elements: list with integer values
     :return: list with indices started from 1
     """
+    return [i for i in range(1, len(elements) + 1)]
 
 
 # ====================================================================================================
@@ -24,6 +25,12 @@ def get_max_element_index(elements: list[int]) -> int | None:
     :param elements: list with integer values
     :return: index of maximum element if exists, None otherwise
     """
+    if len(elements) > 0:
+        mx = 0
+        for i in range(len(elements)):
+            if elements[mx] > elements[i]:
+                mx = i
+        return mx
 
 
 # ====================================================================================================
@@ -34,6 +41,7 @@ def get_every_second_element(elements: list[int]) -> list[int]:
     :param elements: list with integer values
     :return: list with each second element of list
     """
+    return [elements[i] for i in range(1, len(elements), 2)]
 
 
 # ====================================================================================================
@@ -44,6 +52,9 @@ def get_first_three_index(elements: list[int]) -> int | None:
     :param elements: list with integer values
     :return: index of first "3" in the list if exists, None otherwise
     """
+    for i in range(len(elements)):
+        if elements[i] == 3:
+            return i
 
 
 # ====================================================================================================
@@ -54,6 +65,9 @@ def get_last_three_index(elements: list[int]) -> int | None:
     :param elements: list with integer values
     :return: index of last "3" in the list if exists, None otherwise
     """
+    for i in range(len(elements)-1, -1, -1):
+        if elements[i] == 3:
+            return i
 
 
 # ====================================================================================================
@@ -64,6 +78,7 @@ def get_sum(elements: list[int]) -> int:
     :param elements: list with integer values
     :return: sum of elements
     """
+    return sum(elements)
 
 
 # ====================================================================================================
@@ -75,6 +90,7 @@ def get_min_max(elements: list[int], default: int | None) -> tuple[int | None, i
     :param default: default value to return if elements are empty
     :return: (min, max) of list elements or (default, default) if elements are empty
     """
+    return (min(elements), max(elements)) if len(elements) > 0 else (default, default)
 
 
 # ====================================================================================================
@@ -87,3 +103,4 @@ def get_by_index(elements: list[int], i: int, boundary: int) -> int | None:
     :param boundary: boundary for check element value
     :return: element at index `i` from `elements` if element greater then boundary and None otherwise
     """
+    return elements[i] if i < len(elements) and elements[i] > boundary else None
