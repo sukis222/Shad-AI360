338024f7bb0150f9fdf9a1d5f85f59fb02a7af77
@Arthur <artur.makarkin2006@gmail.com>
2024-10-08 17:53:37 +0300

    lets go

--- 02.1.FunctionsStringsIO/tasks/count_util/count_util.py
+++ 02.1.FunctionsStringsIO/tasks/count_util/count_util.py
@@ -19,3 +19,59 @@ def count_util(text: str, flags: str | None = None) -> dict[str, int]:
         * "longest_line" - the longest line length
         * "words" - amount of words
     """
+    fl = []
+    answer = {'chars' : 0, 'lines' : 0, 'words' : 0, 'longest_line' : 0,}
+    real_text = repr(text)
+    print(real_text)
+
+    for elem in text:
+        if elem.isalpha() or elem == ' ':
+            answer['chars'] += 1
+
+    if flags is not None:
+        for elem in flags:
+            if elem == 'm' or elem == 'l' or elem == 'L' or elem == 'w':
+                fl.append(elem)
+
+    answer['lines'] = text.count('\n')
+    answer['chars'] += answer['lines']
+    split_text = text.split('\n')
+    sch = 0
+    for elem in split_text:
+        f = False
+        for i in range(len(elem)):
+            if elem[i] != ' ' and not f:
+                sch += 1
+                f = True
+            elif elem[i] == ' ':
+                f = False
+
+    max_len = 0
+    al_len = 0
+    for elem in split_text:
+        al_len += len(elem)
+        if len(elem) > max_len:
+            max_len = len(elem)
+
+    #answer['lines'] = len(split_text)
+    answer['longest_line'] = max_len
+    answer['words'] = sch - repr(text).count(r'\t')
+    ans = dict()
+    if flags is not None and flags != '':
+         for i in flags:
+             if i == 'm':
+                 ans['chars'] = answer['chars']
+             if i == 'l':
+                 ans['lines'] = answer['lines']
+             if i == 'L':
+                 ans['longest_line'] = answer['longest_line']
+             if i == 'w':
+                 ans['words'] = answer['words']
+    elif flags == '' or flags is None:
+        ans = answer
+        print(ans)
+
+    return ans
+
+#print(count_util(d, ''))
+#print((repr(d).count(r'\t')))
