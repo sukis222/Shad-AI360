cde2a05b293ae1c50fc8179f44c80fb05f219acd
@Arthur <artur.makarkin2006@gmail.com>
2024-10-10 22:47:08 +0300

    first

--- 02.1.FunctionsStringsIO/tasks/git_log/git_log.py
+++ 02.1.FunctionsStringsIO/tasks/git_log/git_log.py
@@ -7,3 +7,13 @@ def reformat_git_log(inp: tp.IO[str], out: tp.IO[str]) -> None:
     Expected input format: `<sha-1>\t<date>\t<author>\t<email>\t<message>`
     Output format: `<first 7 symbols of sha-1>.....<message>`
     """
+    lin = inp.read()
+
+    each_line = lin.split('\n')
+    each_line.pop()
+    for line in each_line:
+        begin = line[0:7]
+        underline = line.split('\t')
+        end = underline[-1]
+        k = '.' * (80 - len(begin) - len(end))
+        out.write(begin + k + end + '\n')
