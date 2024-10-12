4bc5d2481eb7f1066da59350266f57a8f54f85af
@Arthur <artur.makarkin2006@gmail.com>
2024-10-12 11:48:49 +0300

    first

--- 02.1.FunctionsStringsIO/tasks/merge_lists_3/merge_lists_3.py
+++ 02.1.FunctionsStringsIO/tasks/merge_lists_3/merge_lists_3.py
@@ -1,4 +1,5 @@
 import typing as tp
+import heapq
 
 
 def merge(input_streams: tp.Sequence[tp.IO[bytes]], output_stream: tp.IO[bytes]) -> None:
@@ -8,3 +9,16 @@ def merge(input_streams: tp.Sequence[tp.IO[bytes]], output_stream: tp.IO[bytes])
     :param output_stream: output stream. Contains byte-strings separated by "\n". Nonempty stream ends with "\n"
     :return: None
     """
+    s = []
+    heapq.heapify(s)
+
+    for i in range(len(input_streams)):
+        d = input_streams[i].readline()
+        while d != b"":
+            each_byte = int(d.decode())
+            heapq.heappush(s, each_byte)
+            d = input_streams[i].readline()
+
+    while len(s) > 0:
+        ans = heapq.heappop(s)
+        output_stream.write((str(ans) + '\n').encode('utf-8'))
