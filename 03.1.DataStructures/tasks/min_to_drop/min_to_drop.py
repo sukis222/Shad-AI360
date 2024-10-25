76597d0745b8c2952b0d99c21ed115d3ce0023e7
@Arthur <artur.makarkin2006@gmail.com>
2024-10-25 19:56:46 +0300

    first

--- 03.1.DataStructures/tasks/min_to_drop/min_to_drop.py
+++ 03.1.DataStructures/tasks/min_to_drop/min_to_drop.py
@@ -1,4 +1,5 @@
 import typing as tp
+from collections import Counter
 
 
 def get_min_to_drop(seq: tp.Sequence[tp.Any]) -> int:
@@ -6,3 +7,6 @@ def get_min_to_drop(seq: tp.Sequence[tp.Any]) -> int:
     :param seq: sequence of elements
     :return: number of elements need to drop to leave equal elements
     """
+    cnt = Counter(seq)
+    return len(seq) - cnt.most_common()[0][1]
+
