e9098ac7112bc429e915587e4617aa9baf644687
@Arthur <artur.makarkin2006@gmail.com>
2024-10-26 11:37:56 +0300

    first

--- 03.1.DataStructures/tasks/reverse_dict/reverse_dict.py
+++ 03.1.DataStructures/tasks/reverse_dict/reverse_dict.py
@@ -1,8 +1,14 @@
 import typing as tp
-
+import collections
 
 def revert(dct: tp.Mapping[str, str]) -> dict[str, list[str]]:
     """
     :param dct: dictionary to revert in format {key: value}
     :return: reverted dictionary {value: [key1, key2, key3]}
     """
+    dct_2 = collections.defaultdict(list)
+    for elem in dct:
+        dct_2[dct[elem]].append(elem)
+
+    return dict(dct_2)
+
--- 03.1.DataStructures/tasks/traverse_dictionary/traverse_dictionary.py
+++ 03.1.DataStructures/tasks/traverse_dictionary/traverse_dictionary.py
@@ -47,13 +47,39 @@ def traverse_dictionary_iterative(
     :param dct: dictionary of undefined depth with integers or other dicts as leaves with same properties
     :return: list with pairs: (full key from root to leaf joined by ".", value)
     """
-    ans = []
-    for elem in dct:
-        if not isinstance(dct[elem], dict):
-            ans.append((elem, dct[elem]))
+    for key, value in dct.items():
+        if isinstance(value, dict):
+            yield from traverse_dictionary(value)
         else:
-            micro_ans = traverse_dictionary_immutable(dct[elem])
-            for el in micro_ans:
-                ans.append((f'{elem}.{el[0]}', el[1]))
-    return ans
+            yield key, value
+
+
+h = {"a": {"b": {"c": {"d": {"e": 1}}}}}
+f = h
+s = iter({"a": {"b": {"c": {"d": {"e": 1}}}}})
+
+
+
+my_dict = {
+    'key1': {'sub_key1': [1, 2, 3], 'sub_key2': [4, 5, 6]},
+    'key2': {'sub_key3': [7, 8, 9], 'sub_key4': [10, 11, 12]},
+    'deeper_level': {
+        'sub_sub_key1': [{'inner_key': 'value'}, {'inner_key2': 'value2'}],
+        'sub_sub_key2': ['a', 'b', 'c']
+    }
+}
+
+# Функция для обхода словаря любой глубины
+def traverse_dictionary(dictionary):
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            yield from traverse_dictionary(value)
+        else:
+            yield key, value
+
+# Проходимся по всем элементам словаря
+for element in traverse_dictionary(my_dict):
+    print(element)
+
+
 
