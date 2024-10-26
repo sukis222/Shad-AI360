9adebdd6ba7a6e1324d3dc12bb8814b169f52a0e
@Arthur <artur.makarkin2006@gmail.com>
2024-10-26 12:14:01 +0300

    first

--- 03.1.DataStructures/tasks/merge_lists_2/merge_lists.py
+++ 03.1.DataStructures/tasks/merge_lists_2/merge_lists.py
@@ -1,8 +1,22 @@
 import typing as tp
-
+import  heapq
 
 def merge(seq: tp.Sequence[tp.Sequence[int]]) -> list[int]:
     """
     :param seq: sequence of sorted sequences
     :return: merged sorted list
     """
+    my_heap = []
+    ans = []
+    for i in seq:
+        for j in i:
+            heapq.heappush(my_heap, j)
+
+    while len(my_heap) > 0:
+        ans.append(heapq.heappop(my_heap))
+
+    return ans
+
+
+
+
