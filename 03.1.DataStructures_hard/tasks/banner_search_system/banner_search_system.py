9a87793d1ab2011a5e2a025dd3a3eadf9b8a5070
@Arthur <artur.makarkin2006@gmail.com>
2024-10-27 23:35:34 +0300

    first

--- 03.1.DataStructures_hard/tasks/banner_search_system/banner_search_system.py
+++ 03.1.DataStructures_hard/tasks/banner_search_system/banner_search_system.py
@@ -1,5 +1,6 @@
-import re
-from curses.ascii import isalpha
+import heapq
+
+from mypy.checkexpr import defaultdict
 
 
 def normalize(
@@ -10,16 +11,11 @@ def normalize(
     :param text: text to normalize
     :return: normalized query
     """
-
     ans = ''
     for elem in text:
-        if elem.isalpha():
-            ans += elem
-    print(str(ans))
-
-
-    return ans
-
+        if elem.isalpha() or elem.isspace():
+            ans = ans + elem
+    return ans.lower()
 
 
 def get_words(
@@ -30,6 +26,18 @@ def get_words(
     :param query: query to split
     :return: filtered and split query by words
     """
+    sch = 0
+    ans: list[str] = []
+    str_for_work = normalize(query)
+    for i in range(len(str_for_work)):
+        if str_for_work[i] == ' ':
+            if sch > 3:
+                ans.append(str_for_work[i-sch:i])
+            sch = 0
+        else:
+            sch += 1
+
+    return ans
 
 
 def build_index(
@@ -40,7 +48,13 @@ def build_index(
     :param banners: list of banners for indexation
     :return: mapping from word to banners ids
     """
-
+    ans = defaultdict(list)
+    for num in range(len(banners)):
+        mini_list = get_words(normalize(banners[num]))
+        for elem in mini_list:
+            if len(ans[elem]) > 0 and ans[elem][-1] != num or len(ans[elem]) == 0:
+                ans[elem].append(num)
+    return dict(ans)
 
 
 def get_banner_indices_by_query(
@@ -53,6 +67,30 @@ def get_banner_indices_by_query(
     :param index: index to search banners
     :return: list of indices of suitable banners
     """
+    a = []
+    heapq.heapify(a)
+    norm_str = get_words(normalize(query))
+    ans: list[int] = []
+    for elem in norm_str:
+        if elem in index:
+            for sss in index[elem]:
+                heapq.heappush(a, sss)
+    a.sort()
+    sch = 1
+    if len(norm_str) == 1:
+        return a
+    for i in range(1, len(a)):
+        if a[i] == a[i-1]:
+            sch += 1
+        else:
+            if sch == len(norm_str):
+                ans.append(a[i-1])
+            sch = 1
+
+    if sch == len(norm_str):
+        ans.append(a[-1])
+
+    return ans
 
 
 #########################
@@ -77,6 +115,3 @@ def get_banners(
 #########################
 
 
-
-print(normalize("Джинсы со скидка 600р"))
-print(get_words('   kjijji i ii ji jij ji ji ji ji   '))
