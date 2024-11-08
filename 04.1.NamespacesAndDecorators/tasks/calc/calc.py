3721063151d2eb58fa612e7c543bf2b37315843d
@Arthur <artur.makarkin2006@gmail.com>
2024-11-08 23:02:38 +0300

    sec

--- 04.1.NamespacesAndDecorators/tasks/calc/calc.py
+++ 04.1.NamespacesAndDecorators/tasks/calc/calc.py
@@ -10,14 +10,14 @@ def run_calc(context: dict[str, Any] | None = None) -> None:
     sys.stdout.write(PROMPT)
     s = sys.stdin.readline()
     if s:
-        sys.stdout.write(str(eval(s, context)) + '\n')
+        sys.stdout.write(str(eval(s, {"__builtins__" : {}}, context)) + '\n')
     else:
         sys.stdout.write('\n')
     while s:
         sys.stdout.write(f'{PROMPT}')
         s = sys.stdin.readline()
         if s:
-            sys.stdout.write(str(eval(s, context)) + '\n')
+            sys.stdout.write(str(eval(s, {"__builtins__" : {}}, context)) + '\n')
         else:
             sys.stdout.write('\n')
 
@@ -25,4 +25,3 @@ def run_calc(context: dict[str, Any] | None = None) -> None:
 if __name__ == '__main__':
     context = {'math': math}
     run_calc(context)
-
