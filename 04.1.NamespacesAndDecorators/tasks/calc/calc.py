448147b8b89583dc28789fe2ca108215a7bb08c9
@Arthur <artur.makarkin2006@gmail.com>
2024-11-08 21:51:33 +0300

    first

--- 04.1.NamespacesAndDecorators/tasks/calc/calc.py
+++ 04.1.NamespacesAndDecorators/tasks/calc/calc.py
@@ -7,8 +7,22 @@ PROMPT = '>>> '
 
 def run_calc(context: dict[str, Any] | None = None) -> None:
     """Run interactive calculator session in specified namespace"""
+    sys.stdout.write(PROMPT)
+    s = sys.stdin.readline()
+    if s:
+        sys.stdout.write(str(eval(s, context)) + '\n')
+    else:
+        sys.stdout.write('\n')
+    while s:
+        sys.stdout.write(f'{PROMPT}')
+        s = sys.stdin.readline()
+        if s:
+            sys.stdout.write(str(eval(s, context)) + '\n')
+        else:
+            sys.stdout.write('\n')
 
 
 if __name__ == '__main__':
     context = {'math': math}
     run_calc(context)
+
