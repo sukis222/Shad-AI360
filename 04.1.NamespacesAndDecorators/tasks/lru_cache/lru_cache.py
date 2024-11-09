a4d222ae7e578b174da9c456d57ff2387c766d6e
@Arthur <artur.makarkin2006@gmail.com>
2024-11-10 00:22:24 +0300

    aaaaaaaaaaaaa

--- 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
+++ 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
@@ -19,7 +19,7 @@ def cache(max_size: int) -> Callable[[Function], Function]:
     :return: decorator, which wraps any function passed
     """
     cache_of_obj: OrderedDict[str, Any] = OrderedDict()
-    def decor(func: Callable[P, T]) -> Callable[..., Any]:
+    def decor(func: Function) -> Function:
         @wraps(func)
         def wrapper(*args: P.args, **kwacks: P.kwargs) -> T:
             if cache_of_obj.get(str(args) + str(kwacks)) is None:
