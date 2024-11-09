66bfbc835d0b9679de4a017d7be7cdcde9f2c62f
@Arthur <artur.makarkin2006@gmail.com>
2024-11-09 23:07:34 +0300

    first

--- 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
+++ 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
@@ -1,4 +1,6 @@
 from collections.abc import Callable
+from collections import OrderedDict
+from functools import wraps
 from typing import Any, TypeVar
 
 
@@ -12,3 +14,35 @@ def cache(max_size: int) -> Callable[[Function], Function]:
     :param max_size: max amount of unique arguments to store values for
     :return: decorator, which wraps any function passed
     """
+    cache_of_obj = OrderedDict()
+    def decor(func):
+        @wraps(func)
+        def wrapper(*args, **kwacks):
+            if cache_of_obj.get(str(args) + str(kwacks)) is None:
+                #print(cache_of_obj, len(cache_of_obj))
+                while len(cache_of_obj) >= max_size:
+                    for elem in cache_of_obj:
+                        cache_of_obj.pop(elem)
+                        break
+                cache_of_obj[str(args) +  str(kwacks)] = func(*args, **kwacks)
+                cache_of_obj.move_to_end(str(args) +  str(kwacks))
+                return cache_of_obj[str(args) + str(kwacks)]
+            else:
+                return cache_of_obj[str(args) +  str(kwacks)]
+        return wrapper
+
+    return decor
+
+
+
+'''@cache(7)
+def binomial(n: int, k: int) -> int:
+    if k > n:
+        return 0
+    if k == 0:
+        return 1
+    return binomial(n - 1, k) + binomial(n - 1, k - 1)
+
+binomial(8, 4)
+print(help(binomial))'''
+
