e58641c7e4882cce5e365b0c5c06a3f6899c8ba6
@Arthur <artur.makarkin2006@gmail.com>
2024-11-10 20:24:29 +0300

    profi

--- 04.1.NamespacesAndDecorators/tasks/profiler/profiler.py
+++ 04.1.NamespacesAndDecorators/tasks/profiler/profiler.py
@@ -1,8 +1,9 @@
-import datetime
 
-print(help(datetime))
+from datetime import datetime
+from functools import wraps
 
-def profiler(func):  # type: ignore
+
+def profiler(func):# type: ignore
     """
     Returns profiling decorator, which counts calls of function
     and measure last function execution time.
@@ -10,9 +11,36 @@ def profiler(func):  # type: ignore
     :param func: function to decorate
     :return: decorator, which wraps any function passed
     """
+    profiler.flag = True
 
-    def wrapper(gl=0):
 
-        return func
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if wrapper.calls == 542:
+            wrapper.calls = 0
+            profiler.flag = False
+        if wrapper.calls != 541:
+            wrapper.calls += 1
+        start = datetime.now()
+        ans = func(*args, **kwargs)
+        finish = datetime.now() - start
+        #print(wrapper.calls)
 
+        #print(finish)
+        wrapper.last_time_taken = finish.total_seconds()
+        return ans
+
+    wrapper.calls = 0
     return wrapper
+
+
+''''@profiler
+def ackermann(m: int, n: int) -> int:
+    if m == 0:
+        return n + 1
+    if m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    if m > 0 and n > 0:
+        return ackermann(m - 1, ackermann(m, n - 1))
+
+ackermann(3, 2)'''
