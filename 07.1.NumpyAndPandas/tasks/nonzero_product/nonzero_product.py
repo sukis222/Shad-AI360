2baa6c5283dee9fa230aded2e4b36941dc440edc
@Arthur <artur.makarkin2006@gmail.com>
2024-12-11 15:49:31 +0300

    non_zero

--- 07.1.NumpyAndPandas/tasks/nonzero_product/nonzero_product.py
+++ 07.1.NumpyAndPandas/tasks/nonzero_product/nonzero_product.py
@@ -9,3 +9,20 @@ def nonzero_product(matrix: npt.NDArray[np.int_]) -> int | None:
     :param matrix: array,
     :return: product value or None
     """
+
+    E = np.eye(max(matrix.shape[1], matrix.shape[0]))
+    mask = E == 1
+    mask = mask[:matrix.shape[0], :matrix.shape[1]]
+    print(mask)
+
+    sq_mat = matrix[mask]
+    new_mask = sq_mat != 0
+    if new_mask.sum() == 0:
+        return None
+    else:
+        return sq_mat[new_mask].prod()
+
+
+
+X = np.array([[1, 0, 1], [2, 0, 2], [3, 0, 3], [4, 4, 4]])
+print(nonzero_product(X))
