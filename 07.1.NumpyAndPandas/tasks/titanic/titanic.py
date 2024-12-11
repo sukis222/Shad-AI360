3ea1f57208ee19f25a4bbcc717399e3a05ac4b3b
@Arthur <artur.makarkin2006@gmail.com>
2024-12-11 23:58:23 +0300

    base

--- 07.1.NumpyAndPandas/tasks/titanic/titanic.py
+++ 07.1.NumpyAndPandas/tasks/titanic/titanic.py
@@ -2,6 +2,9 @@ import typing as tp
 
 import pandas as pd
 
+from pathlib import Path
+
+
 
 def male_age(df: pd.DataFrame) -> float:
     """
@@ -10,6 +13,9 @@ def male_age(df: pd.DataFrame) -> float:
     :return: mean age
     """
 
+    return df[(df['Sex'] == 'male') & (df['Survived'] == 1) &
+                                  (df['Fare'] > 30) & (df['Embarked'] == 'S') & (df['Age'].notna())]['Age'].mean()
+
 
 def nan_columns(df: pd.DataFrame) -> tp.Iterable[str]:
     """
@@ -17,6 +23,12 @@ def nan_columns(df: pd.DataFrame) -> tp.Iterable[str]:
     :param df: dataframe
     :return: series of columns
     """
+    ans = []
+    for elem in df.keys():
+        mask = df[elem].isna()
+        if mask.sum() > 0:
+            ans.append(elem)
+    return df[ans]
 
 
 def class_distribution(df: pd.DataFrame) -> pd.Series:
@@ -26,6 +38,18 @@ def class_distribution(df: pd.DataFrame) -> pd.Series:
     :return: series with ratios
     """
 
+    gr1 = df[(df['Pclass'] == 1)]['Pclass'].size
+
+    gr2 = df[(df['Pclass'] == 2)]['Pclass'].size
+
+    gr3 = df[(df['Pclass'] == 3)]['Pclass'].size
+
+    grs = gr1 + gr2 + gr3
+
+    ans = pd.Series([gr1/grs, gr2/grs, gr3/grs], index=[1, 2, 3])
+
+    return ans
+
 
 def families_count(df: pd.DataFrame, k: int) -> int:
     """
@@ -34,6 +58,11 @@ def families_count(df: pd.DataFrame, k: int) -> int:
     :param k: number of members,
     :return: number of families
     """
+    happy_family = df['Name'].str.split(',')
+    ans = happy_family.str[0].value_counts()
+    return ans[ans > k].count()
+
+
 
 
 def mean_price(df: pd.DataFrame, tickets: tp.Iterable[str]) -> float:
@@ -43,6 +72,8 @@ def mean_price(df: pd.DataFrame, tickets: tp.Iterable[str]) -> float:
     :param tickets: list of tickets,
     :return: mean fare for this tickets
     """
+    mean_fare = df[df['Ticket'].isin(tickets)]['Fare'].mean()
+    return mean_fare
 
 
 def max_size_group(df: pd.DataFrame, columns: list[str]) -> tp.Iterable[tp.Any]:
@@ -52,7 +83,23 @@ def max_size_group(df: pd.DataFrame, columns: list[str]) -> tp.Iterable[tp.Any]:
     :param columns: columns for grouping,
     :return: list of most common combination
     """
+    max_size = df.groupby(columns).size().idxmax()
+    return max_size
 
+def is_luck(ticket: str) -> bool:
+    """
+    Determine if a ticket is lucky.
+    :param ticket: ticket number
+    :return: True if lucky, False otherwise
+    """
+    if not ticket.isdigit() or len(ticket) % 2 != 0:
+        return False
+
+    half_length = len(ticket) // 2
+    left_side = sum([int(char) for char in ticket[:half_length]])
+    right_side = sum([int(char) for char in ticket[half_length:]])
+
+    return left_side == right_side
 
 def dead_lucky(df: pd.DataFrame) -> float:
     """
@@ -65,3 +112,8 @@ def dead_lucky(df: pd.DataFrame) -> float:
     :param df: dataframe,
     :return: ratio of dead lucky passengers
     """
+    luck = df[df['Ticket'].apply(is_luck)]
+    result = luck['Survived'].value_counts(normalize=True).get(0, 0)
+    return result
+
+
