d2eb177086fdea66c3c6907809026856a7c22fd3
@Arthur <artur.makarkin2006@gmail.com>
2024-12-11 13:11:07 +0300

    first

--- 07.1.NumpyAndPandas/tasks/vander/vander.py
+++ 07.1.NumpyAndPandas/tasks/vander/vander.py
@@ -8,3 +8,10 @@ def vander(array: npt.NDArray[np.float64 | np.int_]) -> npt.NDArray[np.float64]:
     :param array: input array,
     :return: vandermonde matrix
     """
+    ones = np.ones((array.shape[0]))
+
+    array = array.reshape((-1, 1))
+    array = array * ones
+    st = np.arange(0, ones.size)
+    return array ** st
+
