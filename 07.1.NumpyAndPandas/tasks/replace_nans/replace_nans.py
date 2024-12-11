18a3e481320a477673cd3143cd5d5a86b09cd773
@Arthur <artur.makarkin2006@gmail.com>
2024-12-11 15:16:47 +0300

    first

--- 07.1.NumpyAndPandas/tasks/replace_nans/replace_nans.py
+++ 07.1.NumpyAndPandas/tasks/replace_nans/replace_nans.py
@@ -9,3 +9,11 @@ def replace_nans(matrix: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     :param matrix: matrix,
     :return: replaced matrix
     """
+    nan_mask = ~np.isnan(matrix)
+    if (nan_mask).sum() == 0 or matrix.size == 0:
+        return np.zeros(matrix.shape)
+    av = matrix[nan_mask].mean()
+    print(av)
+    return np.where(~nan_mask, av, matrix)
+
+print(replace_nans(np.array([np.nan])))
