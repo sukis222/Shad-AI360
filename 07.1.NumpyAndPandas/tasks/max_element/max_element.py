970b9932e320524928d0d7ee3a20f364f740b540
@Arthur <artur.makarkin2006@gmail.com>
2024-12-11 11:11:54 +0300

    first

--- 07.1.NumpyAndPandas/tasks/max_element/max_element.py
+++ 07.1.NumpyAndPandas/tasks/max_element/max_element.py
@@ -9,3 +9,11 @@ def max_element(array: npt.NDArray[np.int_]) -> int | None:
     :param array: array,
     :return: max element value or None
     """
+    zeroes = np.zeros(array.size, dtype="int32")
+    mask = zeroes == array
+    if (mask.sum() == 1 and mask[-1]) or mask.sum() == 0 or mask.size <= 1:
+        return None
+
+
+    return array[1:][mask[:-1]].max()
+
